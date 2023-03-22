f071a8921c32cba36bcdf69c69d7aa68ecbdbe5f
lilianWarui <lillianwarui09@gmaiil.com>
2023-03-22 12:46:16 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,16 @@
-#include <stdio.h>
+#include "main.h"
 /* execution starts here*/
 int main(void)
 {
-	printf("_putcher");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
-
 }
