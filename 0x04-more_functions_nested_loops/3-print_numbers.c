e120263eec48f21ba1121e99d78c365d669f357f
lilianWarui <lillianwarui09@gmaiil.com>
2023-03-25 12:13:32 -0700

    a function that prints the numbers, from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/* function that prints the numbers, from 0 to 9*/
+void print_numbers(void)
+{
+	/*this is a var c*/
+	char c;
+
+	for (c = '0';c <= '9';c++)
+	{
+		_putchar(c);
+	}
+		_putchar('\n');
+}
