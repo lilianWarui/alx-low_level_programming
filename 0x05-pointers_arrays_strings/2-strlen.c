b6bc4852be5ed471d2340ae5da9e6633097e3ac1
lilianWarui <lillianwarui09@gmaiil.com>
2023-03-28 02:41:10 -0700

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/*
+ * function that returns the length of a string
+ */
+int _strlen(char *s)
+{
+	/*
+	 * var ti check length
+	 */
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	return (length);
+}
